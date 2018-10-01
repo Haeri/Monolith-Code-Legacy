8801ab0fae5a223eb7f2acf740134a16cc570dbf
Haeri <Haeri@users.noreply.github.com>
2018-10-01 23:43:32 +0200

    fixed?

--- deployer.sh
+++ deployer.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cp "./build/libs/monolith.jar" "./Package/monolith.jar"
-zip -r "package.zip" "./Package"
-ls -a
-curl -F "key=$1" -F "version=1.0.0" -F "build=60" -F "beta=false" -F "need_fresh=false" -F "release_notes=Some random notes to be honest" -F "file=@Package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
+zip -r "package.zip" "./Package/"
+curl -F "key=$1" -F "version=1.0.0" -F "build=60" -F "beta=false" -F "need_fresh=false" -F "release_notes=Some random notes to be honest" -F "file=@package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
