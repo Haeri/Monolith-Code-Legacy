66e68fdac7133ed7c631658cb51a3f4f737e7b70
Haeri <Haeri@users.noreply.github.com>
2018-10-02 01:13:23 +0200

    fuuuu

--- deployer.sh
+++ deployer.sh
@@ -9,10 +9,10 @@ zip -r "../package.zip" *
 cd ..
 
 # read relese values from file
-version=(grep -o "^version:(.*)$" release.txt
-build=(grep -o "^build:(.*)$" release.txt
-beta=(grep -o "^beta:(.*)$" release.txt
-refresh=(grep -o "^refresh:(.*)$" release.txt
+version=(grep -o "^version:(.*)$" release.txt)
+build=(grep -o "^build:(.*)$" release.txt)
+beta=(grep -o "^beta:(.*)$" release.txt)
+refresh=(grep -o "^refresh:(.*)$" release.txt)
 
 echo "version: $version, build: $build, beta: $beta, refresh: $refresh"
 
