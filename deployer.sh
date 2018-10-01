136b6418c9124a30a01630e2aea41f48fc67c231
Haeri <Haeri@users.noreply.github.com>
2018-10-01 23:13:39 +0200

    Autodeployer

---
+++ deployer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp "/build/libs/monolith.jar" "/Package/monolith.jar"
+zip "/Package"
+curl -F "key=$1" -F "version=1.0.0" -F "build=60" -F "beta=false" -F "need_fresh=false" -F "release_notes=Some random notes to be honest" -F "file=@Package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
