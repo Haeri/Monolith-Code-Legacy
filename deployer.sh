db047999473eb05e198f96b9d2206bd229fdb694
Haeri <Haeri@users.noreply.github.com>
2018-10-02 00:06:04 +0200

    fancy

--- deployer.sh
+++ deployer.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 cp "./build/libs/monolith.jar" "./Package/monolith.jar"
-zip -r "package.zip" "./Package/"
-curl -F "key=$1" -F "version=1.0.0" -F "build=60" -F "beta=false" -F "need_fresh=false" -F "release_notes=Some random notes to be honest" -F "file=@package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
+zip -r "package.zip" "./Package/*"
+read version < "./release.txt"
+read build < "./release.txt"
+read beta < "./release.txt"
+read refresh < "./release.txt"
+echo "version: $version, build: $build, beta: $beta, refresh: $refresh"
+curl -F "key=$1" -F "version=$version" -F "build=$build" -F "beta=$beta" -F "need_fresh=$fresh" -F "release_notes=Some random notes to be honest" -F "file=@package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
