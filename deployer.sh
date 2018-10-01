72d5587590e5a401dae50c2bc98bfecdc0b0b020
Haeri <Haeri@users.noreply.github.com>
2018-10-01 23:33:10 +0200

    debuging

--- deployer.sh
+++ deployer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
-cp "/build/libs/monolith.jar" "/Package/monolith.jar"
-zip "/Package"
+echo $1
+ls -a
+cp "./build/libs/monolith.jar" "./Package/monolith.jar"
+zip "./Package"
+ls -a
 curl -F "key=$1" -F "version=1.0.0" -F "build=60" -F "beta=false" -F "need_fresh=false" -F "release_notes=Some random notes to be honest" -F "file=@Package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
