471056e42414caf29a6f3c90f81238270d1eb165
Haeri <Haeri@users.noreply.github.com>
2018-10-02 00:24:18 +0200

    try again...

--- deployer.sh
+++ deployer.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
+
+# copy jar file
 cp "./build/libs/monolith.jar" "./Package/monolith.jar"
-zip -r "package.zip" "./Package/*"
-read version < "./release.txt"
-read build < "./release.txt"
-read beta < "./release.txt"
-read refresh < "./release.txt"
+
+# zip package
+cd "./Package"
+zip -r "../package.zip" *
+cd ..
+
+# read relese values from file
+file < "./release.txt"
+version = read file
+build = read file
+beta = read file
+refresh = read file
+
 echo "version: $version, build: $build, beta: $beta, refresh: $refresh"
+
+# send package to server
 curl -F "key=$1" -F "version=$version" -F "build=$build" -F "beta=$beta" -F "need_fresh=$fresh" -F "release_notes=Some random notes to be honest" -F "file=@package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
