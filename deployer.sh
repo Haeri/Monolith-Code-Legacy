45b37e4b4276beb5a38776bef60ef87937797a43
Haeri <Haeri@users.noreply.github.com>
2018-10-02 13:51:50 +0200

    tell me why

--- deployer.sh
+++ deployer.sh
@@ -15,5 +15,8 @@ beta=$(grep -oP "^beta:\K(.*)$" release.txt)
 refresh=$(grep -oP "^refresh:\K(.*)$" release.txt)
 text=$(tail -n +5 release.txt)
 
+echo "version: $version, build: $build, beta: $beta, refresh: $refresh\n"
+echo "$text"
+
 # send package to server
-curl -F "key=$1" -F "version=$version" -F "build=$build" -F "beta=$beta" -F "need_fresh=$fresh" -F "release_notes=Some random notes to be honest" -F "file=@package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
+curl -F "key=$1" -F "version=$version" -F "build=$build" -F "beta=$beta" -F "need_fresh=$fresh" -F "release_notes=$text" -F "file=@package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
