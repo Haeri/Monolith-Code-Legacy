7a7ef4a6f77ca6891d22b4bf915f70b23418b14d
Haeri <Haeri@users.noreply.github.com>
2018-10-02 13:45:00 +0200

    This is it!!!!

--- deployer.sh
+++ deployer.sh
@@ -9,12 +9,11 @@ zip -r "../package.zip" *
 cd ..
 
 # read relese values from file
-version=$(grep -o "^version:(.*)$" release.txt)
-build=$(grep -o "^build:(.*)$" release.txt)
-beta=$(grep -o "^beta:(.*)$" release.txt)
-refresh=$(grep -o "^refresh:(.*)$" release.txt)
-
-echo "version: $version, build: ${build}, beta: $beta, refresh: $refresh"
+version=$(grep -oP "^version:\k(.*)$" release.txt)
+build=$(grep -oP "^build:\K(.*)$" release.txt)
+beta=$(grep -oP "^beta:\K(.*)$" release.txt)
+refresh=$(grep -oP "^refresh:\K(.*)$" release.txt)
+text=$(tail -n +5 release.txt)
 
 # send package to server
 curl -F "key=$1" -F "version=$version" -F "build=$build" -F "beta=$beta" -F "need_fresh=$fresh" -F "release_notes=Some random notes to be honest" -F "file=@package.zip" http://monolith-code.net.tiberius.sui-inter.net/release/addRelease
