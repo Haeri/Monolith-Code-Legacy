b3e2143f7128d1a358ffae62696e879e306744a2
Haeri <Haeri@users.noreply.github.com>
2018-10-19 17:57:35 +0200

    Updater Fix

--- src/main/java/net/monolith/Updater.java
+++ src/main/java/net/monolith/Updater.java
@@ -1,77 +1,54 @@
 package net.monolith;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 
-import javax.swing.JOptionPane;
+public class Updater extends Thread{
 
-public class Updater {
-	private static String newVersion = "";
-	private static int newBuild = -1;
-	
-	private final static String WEB_ADDRESS = "http://monolith-code.net.tiberius.sui-inter.net/update/";
-	
-	public static boolean checkUpdate(MonolithFrame parent){
-		if(checkConnectin(parent.console)){
-			if(!compareVersions()){
-				int res = JOptionPane.showOptionDialog(parent, "There is a new version available. Would you like to update?\nold:  Monolith Text v" + GlobalVariables.VERSION + " : " + GlobalVariables.BUILD + "\nnew: Monolith Text v" + newVersion + " : " + newBuild, "New Vesion Available", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
-				if(res == JOptionPane.OK_OPTION){
-					return true;
-				}
-			}
-		}
-		return false;
+	private MonolithFrame motherFrame;
+
+	public Updater(MonolithFrame parent){
+		motherFrame = parent;
 	}
-	
-	// Gets latest version
-	private static boolean compareVersions() {
-		// fetch new version number
-		String version = "";
-		String build = "";
-     
-		try{
-			URL url = new URL(WEB_ADDRESS + "version.txt");
-		
-			// Get the input stream through URL Connection
-		    URLConnection con = url.openConnection();
-		    InputStream is =con.getInputStream();
-		
-		    BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		    version = br.readLine();
-		    build = br.readLine();
-		
-		}catch (IOException e){
-			return true;
-		}
-		
-		newVersion = version;
-		newBuild = Integer.parseInt(build);
-		
-		return (GlobalVariables.BUILD >= newBuild);
-	}
-	
-	// Checks internet connection
-	public static boolean checkConnectin(Console console) {
+
+	public void run() {
+		// Delay check
 		try {
-			try {
-				URL url = new URL("http://www.google.com");
-				HttpURLConnection con = (HttpURLConnection) url.openConnection();
-				con.connect();
-				if (con.getResponseCode() == 200){
-					return true;
-				}
-			} catch (Exception exception) {
-				return false;
-			}
-		} catch (Exception e) {
-			console.println(e.getMessage(), Console.err);
+			Thread.sleep(600);
+		} catch (InterruptedException e) {
+			if (GlobalVariables.debug)
+				e.printStackTrace();
+		}
+
+		shouldUpdate();
+	}
+
+	public void shouldUpdate(){
+		int ret = 0;
+		try {
+			String tmp = execCmd("java -jar Updater.jar " + GlobalVariables.VERSION + " " + GlobalVariables.BUILD + " true");
+			tmp = tmp.replace("\n", "").replace("\r", "");
+			ret = Integer.parseInt(tmp);
+		}catch(Exception e){
+			motherFrame.console.println(e.getMessage(), Console.err);
 			if(GlobalVariables.debug) e.printStackTrace();
 		}
-		return false;
+
+		if(ret == 1){
+			updateMonolith();
+		}
+	}
+
+	public void updateMonolith() {
+		try {
+			Runtime.getRuntime().exec("java -jar Updater.jar " + GlobalVariables.VERSION + " " + GlobalVariables.BUILD + " false");
+		} catch (IOException e) {
+			motherFrame.console.println(e.getMessage(), Console.err);
+			if(GlobalVariables.debug) e.printStackTrace();
+		}
+	}
+
+	private static String execCmd(String cmd) throws java.io.IOException {
+		java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
+		return s.hasNext() ? s.next() : "";
 	}
 }
